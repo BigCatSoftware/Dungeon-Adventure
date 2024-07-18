c1efea5a9a27c744280f6fc9b8a50ca166123dde
Nazarii Revitskyi <nazarii@uw.edu>
2024-07-17 19:25:45 -0700

    fixed bug with random out of bounds for Name generator, added name generator tests for warrior and priestess, thief under development

--- core/src/test/TestNameGenerator.java
+++ core/src/test/TestNameGenerator.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import Model.NameGenerator;
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,38 @@ public class TestNameGenerator {
         String strNameGeneratorData = myNameGenerator.toString();
         assertFalse(strNameGeneratorData.isEmpty());
         System.out.println(strNameGeneratorData);
+    }
+    @Test
+    void testWarriorName(){
+        String warriorNames = "Skeithvirr The Wolf, Rhaumm The Tower, Hrurn The Ironclad," +
+            " Tirstald The Vengeful, Brorkadrumm The Sentinel, Aesbrekr The Thunder, " +
+            "Ornsamm The Defiant, Drotrin The Bloody, Sgolvammenn The Maneater," +
+            " Ignedr The Undying";
+        String randomWarriorName = myNameGenerator.getWarriorName();
+        assertTrue(warriorNames.contains(randomWarriorName));
+        System.out.println("\nThe warrior name: " +
+            randomWarriorName + " exists in a list of possible warrior names.\n" + warriorNames);
+    }
+    @Test
+    void testPriestessName(){
+        String priestessNames = "Ella the Loving, Nissa the Gracious, Valorie the Devoted, " +
+            "Leeta the Faithful, Rosalind the Valiant, Tara the Honest, Melanie the Caring, " +
+            "Helen the Lionheart, Arianna the Righteous, Yavia the Reliable";
+        String randomPriestessName = myNameGenerator.getPriestessName();
+        assertTrue(priestessNames.contains(randomPriestessName));
+        System.out.println("\nThe priestess name: " +
+            randomPriestessName + " exists in a list of possible priestess names.\n"
+            + priestessNames);
+    }
+    @Test
+    void testThiefName(){
+        String priestessNames = "Ella the Loving, Nissa the Gracious, Valorie the Devoted, " +
+            "Leeta the Faithful, Rosalind the Valiant, Tara the Honest, Melanie the Caring, " +
+            "Helen the Lionheart, Arianna the Righteous, Yavia the Reliable";
+        String randomPriestessName = myNameGenerator.getPriestessName();
+        assertTrue(priestessNames.contains(randomPriestessName));
+        System.out.println("\nThe priestess name: " +
+            randomPriestessName + " exists in a list of possible priestess names.\n"
+            + priestessNames);
     }
 }
