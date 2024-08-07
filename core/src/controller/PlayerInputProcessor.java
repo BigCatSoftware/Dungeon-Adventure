bf6761c82f2e1fd82a8809fe101887922600f8ae
AlvVal282 <alvarovd@outlook.com>
2024-08-07 15:00:53 -0700

    Nazarii_BRANCH merge

--- core/src/controller/PlayerInputProcessor.java
+++ core/src/controller/PlayerInputProcessor.java
@@ -1,6 +1,8 @@
 package controller;
 
 import model.GameMaster;
+import view.CombatScreen;
+import view.GameScreen;
 import view.SettingsScreen;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
@@ -16,7 +18,7 @@ import static com.dungeonadventure.game.DungeonAdventure.*;
  */
 public class PlayerInputProcessor extends InputAdapter {
     private final DungeonAdventure myGame;
-    private final Screen myPreviousScreen;
+    private final GameScreen myPreviousScreen;
 
     /**
      * Constructs a new PlayerInputProcessor.
@@ -24,33 +26,9 @@ public class PlayerInputProcessor extends InputAdapter {
      * @param theGame the main game instance
      * @param thePreviousScreen the previous screen to return to
      */
-    public PlayerInputProcessor(final DungeonAdventure theGame, final Screen thePreviousScreen) {
+    public PlayerInputProcessor(final DungeonAdventure theGame, final GameScreen thePreviousScreen) {
         myGame = theGame;
         myPreviousScreen = thePreviousScreen;
-    }
-
-    /**
-     * Handles touch down events on the screen, specifically for navigating to the settings screen.
-     *
-     * @param screenX the x-coordinate of the touch
-     * @param screenY the y-coordinate of the touch
-     * @param pointer the pointer for the event
-     * @param button the button for the event
-     * @return true if the touch event is handled, false otherwise
-     */
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        // Convert screen coordinates to match the game's coordinate system
-        int x = DungeonAdventure.WIDTH - SETTINGS_BUTTON_WIDTH;
-        int y = DungeonAdventure.HEIGHT - SETTINGS_BUTTON_Y - SETTINGS_BUTTON_HEIGHT;
-
-        if (screenX >= x && screenX <= x + SETTINGS_BUTTON_WIDTH &&
-                screenY >= y && screenY <= y + SETTINGS_BUTTON_HEIGHT) {
-            myGame.setScreen(new SettingsScreen(myGame, myPreviousScreen));
-            return true; // Indicates that the touch event was handled
-        }
-
-        return super.touchDown(screenX, screenY, pointer, button);
     }
 
     /**
@@ -68,29 +46,40 @@ public class PlayerInputProcessor extends InputAdapter {
                 if(gm.getMap()[gm.getPlayerX()][gm.getPlayerY()+1].isWalkable()){
                     gm.getPlayer().moveCharacterUp();
                 }
+                if(GameMaster.getInstance().isHeroNearEnemy()){
+                    myGame.setScreen(new CombatScreen(myGame, myPreviousScreen));
+                }
                 break;
             case Input.Keys.DOWN:
                 if(gm.getMap()[gm.getPlayerX()][gm.getPlayerY()-1].isWalkable()){
                     gm.getPlayer().moveCharacterDown();
+                }
+                if(GameMaster.getInstance().isHeroNearEnemy()){
+                    myGame.setScreen(new CombatScreen(myGame, myPreviousScreen));
                 }
                 break;
             case Input.Keys.LEFT:
                 if(gm.getMap()[gm.getPlayerX()-1][gm.getPlayerY()].isWalkable()){
                     gm.getPlayer().moveCharacterLeft();
                 }
+                if(GameMaster.getInstance().isHeroNearEnemy()){
+                    myGame.setScreen(new CombatScreen(myGame, myPreviousScreen));
+                }
                 break;
             case Input.Keys.RIGHT:
                 if(gm.getMap()[gm.getPlayerX()+1][gm.getPlayerY()].isWalkable()){
                     gm.getPlayer().moveCharacterRight();
                 }
+                if(GameMaster.getInstance().isHeroNearEnemy()){
+                    myGame.setScreen(new CombatScreen(myGame, myPreviousScreen));
+                }
                 break;
-            case Input.Keys.SPACE:
-                gm.getPlayer().attack();
-                //TODO: place combat screen to test combat.
-                break;
+            case Input.Keys.ESCAPE:
+                myPreviousScreen.showMenu();
             default:
                 return false; // Indicates that the key event was not handled
         }
+        myPreviousScreen.setPlayerImagePosition();
         return true; // Indicates that the key event was handled
     }
 }
--- core/src/view/GameScreen.java
+++ core/src/view/GameScreen.java
@@ -1,20 +1,33 @@
 package view;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import controller.PlayerInputProcessor;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.dungeonadventure.game.DungeonAdventure;
+import model.Enemy;
 import model.GameMaster;
-import model.Position;
+import model.Gremlin;
+import model.Ogre;
 import model.Priestess;
+import model.Skeleton;
 import model.Thief;
+import model.Tile;
 import model.Warrior;
+import static com.dungeonadventure.game.DungeonAdventure.myBackgroundMusic;
 
-import static com.dungeonadventure.game.DungeonAdventure.SETTINGS_BUTTON_Y;
-import static com.dungeonadventure.game.DungeonAdventure.SETTINGS_BUTTON_HEIGHT;
-import static com.dungeonadventure.game.DungeonAdventure.SETTINGS_BUTTON_WIDTH;
+import java.util.ArrayList;
 
 /**
  * Represents the main game screen where the dungeon is rendered and the player interacts with the game.
@@ -23,12 +36,27 @@ import static com.dungeonadventure.game.DungeonAdventure.SETTINGS_BUTTON_WIDTH;
  */
 public class GameScreen implements Screen {
     private static final int TILE_SIZE = 16;
-
+    private static final int BUTTON_WIDTH = 154;
+    private static final int BUTTON_HEIGHT = 54;
+    private static final int BUTTON_Y_OFFSET = 10;
+    private static final int BUTTON_X_OFFSET = 77;
+    private final Viewport myViewPort;
+    private final Stage myStage;
+    private final Table myGameTable;
+    private final Table myGameMenuTable;
+    private final BitmapFont myFont;
     private final DungeonAdventure myGame;
     private final Texture mySettingsButtonActive;
     private final Texture mySettingsButtonInactive;
-    private final Texture myPlayerTexture;
-    private final DungeonRenderer myDungeonRenderer;
+    private final Image myPlayerImage;
+    private final Texture myWallTexture;
+    private final Texture myDoorTexture;
+    private final Texture myFloorTexture;
+    private final Texture myGremlinTexture;
+    private final Texture mySkeletonTexture;
+    private final Texture myOgreTexture;
+    private boolean myMenuShown = false;
+    //private final DungeonRenderer myDungeonRenderer;
 
     /**
      * Constructs a new GameScreen.
@@ -37,12 +65,37 @@ public class GameScreen implements Screen {
      */
     public GameScreen(final DungeonAdventure theGame) {
         myGame = theGame;
+        myBackgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("NightAmbianceLoop.ogg"));
+        myBackgroundMusic.play();
+        myBackgroundMusic.setLooping(true);
+        myViewPort = new ScreenViewport();
+        myStage = new Stage(myViewPort, myGame.batch);
+        myGameTable = new Table();
+        myGameMenuTable = new Table();
+        myFont = new BitmapFont(Gdx.files.internal("fonts/alagard.fnt"), Gdx.files.internal("fonts/alagard.png"), false);
         mySettingsButtonActive = new Texture("SettingsActive.png");
         mySettingsButtonInactive = new Texture("SettingsInactive.png");
-        myPlayerTexture = initPlayerTexture();
-        myDungeonRenderer = new DungeonRenderer();
+        myWallTexture = new Texture("wall.png");
+        myDoorTexture = new Texture("door.png");
+        myFloorTexture = new Texture("floor.png");
+        myGremlinTexture = new Texture("Pixel Gremlin.png");
+        mySkeletonTexture = new Texture("Pixel Skeleton.png");
+        myOgreTexture = new Texture("Pixel Ogre.png");
+        myGameTable.setFillParent(true);
+        myStage.addActor(myGameTable);
+        //add to table
+        myPlayerImage = initPlayerTexture();
+        myPlayerImage.setPosition(GameMaster.getInstance().getPlayerX()*TILE_SIZE, GameMaster.getInstance()
+                .getPlayerY()*TILE_SIZE);
+        myGameMenuTable.setBounds((float) DungeonAdventure.WIDTH / 4,((float) DungeonAdventure.HEIGHT / 4), (float) DungeonAdventure.WIDTH / 2, (float) DungeonAdventure.HEIGHT / 2);
+        myGameMenuTable.setBackground(new TextureRegionDrawable(new Texture("GameMenuBackground.png")));
+        myGameMenuTable.setVisible(false);
+        initGameMenuButtons();
+        myGameTable.addActor(myGameMenuTable);
+        myGameTable.addActor(myPlayerImage);
+        //myDungeonRenderer = new DungeonRenderer();
     }
-    private Texture initPlayerTexture(){
+    private Image initPlayerTexture(){
         Texture playerTexture = null;
         if(GameMaster.getInstance().getPlayer() instanceof Warrior){
             playerTexture = new Texture("Pixel Warrior.png");
@@ -57,7 +110,7 @@ public class GameScreen implements Screen {
             throw new IllegalArgumentException("Can't determine hero class from game master to" +
                     " draw its texture");
         }
-        return playerTexture;
+        return new Image(playerTexture);
     }
     /**
      * Called when the screen is shown.
@@ -77,33 +130,121 @@ public class GameScreen implements Screen {
     @Override
     public void render(final float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
-        myGame.batch.begin();
+        //myGame.batch.begin();
 
         // Render the dungeon
-        myDungeonRenderer.render(myGame.batch);
-        // Render character
-        myGame.batch.draw(myPlayerTexture, getPlayerPos().getMyX()*TILE_SIZE,getPlayerPos().getMyY()*TILE_SIZE,TILE_SIZE,TILE_SIZE);
-        settingsButtonDraw();
-
+        myGame.batch.begin();
+        initMap();
+        initEntities();
         myGame.batch.end();
+        myStage.act(Gdx.graphics.getDeltaTime());
+        myStage.draw();
+        //myGame.batch.end();
     }
-
-    /**
-     * Draws the settings button.
-     * The button changes appearance based on whether it is hovered over or not.
-     */
-    private void settingsButtonDraw() {
-        int x = DungeonAdventure.WIDTH - SETTINGS_BUTTON_WIDTH;
-        if (Gdx.input.getX() < x + SETTINGS_BUTTON_WIDTH && Gdx.input.getX() > x &&
-                DungeonAdventure.HEIGHT - Gdx.input.getY() < SETTINGS_BUTTON_Y + SETTINGS_BUTTON_HEIGHT &&
-                DungeonAdventure.HEIGHT - Gdx.input.getY() > SETTINGS_BUTTON_Y) {
-            myGame.batch.draw(mySettingsButtonActive, x, SETTINGS_BUTTON_Y, SETTINGS_BUTTON_WIDTH, SETTINGS_BUTTON_HEIGHT);
-        } else {
-            myGame.batch.draw(mySettingsButtonInactive, x, SETTINGS_BUTTON_Y, SETTINGS_BUTTON_WIDTH, SETTINGS_BUTTON_HEIGHT);
+    private void initMap(){
+        Tile[][] map = GameMaster.getInstance().getMap();
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                Texture texture = null;
+                if (map[i][j] == Tile.WALL) {
+                    texture = myWallTexture;
+                } else if (map[i][j] == Tile.FLOOR) {
+                    texture = myFloorTexture;
+                } else if (map[i][j] == Tile.DOOR) {
+                    texture = myDoorTexture;
+                }
+                if (texture != null) {
+                    myGame.batch.draw(texture, i*TILE_SIZE, j*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
+                else{
+                    throw new IllegalStateException("map image is set to null!");
+                }
+            }
         }
     }
-    private Position getPlayerPos(){
-        return GameMaster.getInstance().getPlayer().getPosition();
+    private void initEntities(){
+        final ArrayList<Enemy> list = GameMaster.getInstance().getAllEnemies();
+        for(Enemy e : list){
+            if(e instanceof Gremlin){
+                myGame.batch.draw(myGremlinTexture, e.getPosition().getMyX()*TILE_SIZE, e.getPosition().getMyY()*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            }
+            else if(e instanceof Skeleton){
+                myGame.batch.draw(mySkeletonTexture, e.getPosition().getMyX()*TILE_SIZE, e.getPosition().getMyY()*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            }
+            else if(e instanceof Ogre){
+                myGame.batch.draw(myOgreTexture, e.getPosition().getMyX()*TILE_SIZE, e.getPosition().getMyY()*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            }
+            else{
+                throw new IllegalStateException("Unknown enemy type when drawing on map. Entity name: " + e.getClass().getSimpleName());
+            }
+        }
+    }
+    public void setPlayerImagePosition(){
+        myPlayerImage.setPosition(GameMaster.getInstance().getPlayerX()*TILE_SIZE, GameMaster.getInstance().getPlayerY()*TILE_SIZE);
+    }
+    private void initGameMenuButtons(){
+        final TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style.font = myFont;
+        style.up = new TextureRegionDrawable(new Texture("CombatButton.Up.png"));
+        style.down = new TextureRegionDrawable(new Texture("CombatButton.Down.png"));
+        style.disabled = new TextureRegionDrawable(new Texture("CombatButton.Disabled.png"));
+        initResumeButton(style);
+        initSettingsButton(style);
+        initMenuButton(style);
+    }
+    private void initResumeButton(final TextButton.TextButtonStyle theStyle){
+        final TextButton button = new TextButton("RESUME", theStyle);
+        button.setBounds(myGameMenuTable.getX() - BUTTON_X_OFFSET, myGameMenuTable.getY()*2 - (2 * BUTTON_HEIGHT)-(BUTTON_Y_OFFSET), BUTTON_WIDTH, BUTTON_HEIGHT);
+        button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.audio.newSound(Gdx.files.internal("sounds/button.ogg")).play();
+                showMenu();
+            }
+        });
+        myGameMenuTable.addActor(button);
+    }
+    private void initSettingsButton(final TextButton.TextButtonStyle theStyle){
+        final TextButton button = new TextButton("SETTINGS", theStyle);
+        button.setBounds(myGameMenuTable.getX() - BUTTON_X_OFFSET, myGameMenuTable.getY()*2 - (3 * BUTTON_HEIGHT)-(2* BUTTON_Y_OFFSET), BUTTON_WIDTH, BUTTON_HEIGHT);
+        button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.audio.newSound(Gdx.files.internal("sounds/button.ogg")).play();
+                myGame.setScreen(new SettingsScreen(myGame, GameScreen.this));
+                myGameMenuTable.setVisible(false);
+                myMenuShown = false;
+            }
+        });
+        myGameMenuTable.addActor(button);
+    }
+    private void initMenuButton(final TextButton.TextButtonStyle theStyle){
+        final TextButton button = new TextButton("EXIT TO MENU", theStyle);
+        button.setBounds(myGameMenuTable.getX() - BUTTON_X_OFFSET, myGameMenuTable.getY()*2 - (4 * BUTTON_HEIGHT) - (3 * BUTTON_Y_OFFSET), BUTTON_WIDTH, BUTTON_HEIGHT);
+        button.getLabel().setFontScale(0.7f);
+        button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.audio.newSound(Gdx.files.internal("sounds/button.ogg")).play();
+                dispose();
+                myGame.setScreen(new MainMenuScreen(myGame));
+                //TODO: auto save and go to menu
+                GameMaster.getInstance().restart();
+            }
+        });
+        myGameMenuTable.addActor(button);
+    }
+    public void showMenu(){
+        if(myMenuShown){
+            myGameMenuTable.setVisible(false);
+            Gdx.input.setInputProcessor(new PlayerInputProcessor(myGame, GameScreen.this));
+            myMenuShown = false;
+        }
+        else{
+            myGameMenuTable.setVisible(true);
+            Gdx.input.setInputProcessor(myStage);
+            myMenuShown = true;
+        }
     }
     /**
      * Called when the screen is resized.
@@ -147,6 +288,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         Gdx.input.setInputProcessor(null);
+        myBackgroundMusic.dispose();
         mySettingsButtonActive.dispose();
         mySettingsButtonInactive.dispose();
         // Dispose of other resources if needed
